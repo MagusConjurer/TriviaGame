86208dee23800f0ebb5bf7ad15ec64a44c45b25f
MagusConjurer <cameronisadavis@gmail.com>
2020-03-28 16:41:19 -0600

    The updatePage function now adds the quote, answers, and shows whether selected answer is correct

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,136 +10,136 @@ var noAnswer = 0;
 quoteOne = {
     quote: "The wheel is come full circle.",
     answers: [
-        {answerOne: "King Lear", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "King Lear", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteTwo = {
     quote: "Better three hours too soon than a minute too late.",
     answers: [
-        {answerOne: "The Merry Wives of Windsor", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "The Merry Wives of Windsor", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteThree = {
     quote: "Cowards die many times before their deaths; The valiant never taste of death but once.",
     answers: [
-        {answerOne: "Julius Ceasar", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "Julius Ceasar", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteFour = {
     quote: "My words fly up, my thoughts remain below. Words without thoughts never to heaven go.",
     answers: [
-        {answerOne: "Hamlet", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "Hamlet", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteFive = {
     quote: "Brevity is the soul of wit.",
     answers: [
-        {answerOne: "Hamlet", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "Hamlet", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteSix = {
     quote: "Men of few words are the best men",
     answers: [
-        {answerOne: "Henry V", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "Henry V", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteSeven = {
     quote: "All that glitters is not gold.",
     answers: [
-        {answerOne: "The Merchant of Venice", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "The Merchant of Venice", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteEight = {
     quote: "Love all, trust a few, do wrong to none.",
     answers: [
-        {answerOne: "All's Well That Ends Well", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "All's Well That Ends Well", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteNine = {
     quote: "Our doubts are traitors and make us lose the good we oft might win by fearing to attempt.",
     answers: [
-        {answerOne: "Measure for Measure", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "Measure for Measure", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteTen = {
     quote: "I was seeking for a fool when I found you",
     answers: [
-        {answerOne: "As You Like It", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "As You Like It", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteEleven = {
     quote: "Out of my sight! Thou dost infect my eyes!",
     answers: [
-        {answerOne: "Richard III", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "Richard III", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteTwelve = {
     quote: "What, you egg!",
     answers: [
-        {answerOne: "Macbeth", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "Macbeth", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteThirteen = {
     quote: "I would challenge you to a battle of wits, but I see you are unarmed.",
     answers: [
-        {answerOne: "The Taming of the Shrew", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "The Taming of the Shrew", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteFourteen = {
     quote: "I am not bound to please thee with my answers.",
     answers: [
-        {answerOne: "Merchant of Venice", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "Merchant of Venice", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 quoteFifteen = {
     quote: "Hell is empty. And all the devils are here.",
     answers: [
-        {answerOne: "The Tempest", isAnswer: true},
-        {answerTwo: "", isAnswer: false},
-        {answerThree: "", isAnswer: false},
-        {answerFour: "", isAnswer: false}
+        {answer: "The Tempest", isAnswer: true},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false},
+        {answer: "", isAnswer: false}
     ]
 };
 
@@ -152,36 +152,50 @@ var trivia = {
 
     // Function to select quote
     startGame : function(){
-        this.selectQuotes(this.quotes, selectedQuotes);
-        console.log(selectedQuotes);
+        this.selectQuotes();
+        this.updatePage(selectedQuotes[0]);
+    
     },
 
-    selectQuotes : function(quotes, selected){
+    selectQuotes : function(){
         // Select 5 non-repeating random indexes
         var randNums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-        for(i=0; i < quotes.length; i++){
+        for(var i=0; i < this.quotes.length; i++){
             var index = Math.floor(Math.random() * (i + 1));
-            [randNums[i], randNums[index]] = [randNums[index], randNums[i]];
-        }
+            [randNums[i], randNums[index]] = [randNums[index], randNums[i]];   
+        };
         // Go through array of quotes
-        for(i=0; i < 5; i++){
+        for(var j=0; j < 5; j++){
             // Add 5 random quotes to a new array
-            selected.push(quotes[randNums[i]]);
-        }
-    },
-
-    checkAnswer : function(){
-        // Take clicked answer and check if it ids the answer
-
-        // Highlight selected answer red or green base on isAnswer
+            selectedQuotes.push(this.quotes[randNums[j]]);
+        };
     },
 
     // Function to update page with currentquote details
-    updatePage : function(quote){
-        // Start at index one of selected quotes
-
-        // Change quote and answers elements
-    }
+    updatePage : function(item){
+         // Update the quote with selected quote
+        $("#quote").html("\"" + item.quote + "\"");
+        $("#answerOne").html(item.answers[0].answer);
+        $("#answerTwo").html(item.answers[1].answer);
+        $("#answerThree").html(item.answers[2].answer);
+        $("#answerFour").html(item.answers[3].answer);
+        var answers = item.answers;
+        var hasClicked = false;
+        $(".answer").on("click", function(){
+            var clicked = $(this).text();
+            if(hasClicked == false){
+                for(var i = 0; i < answers.length; i++){
+                    if(answers[i].answer === clicked && answers[i].isAnswer == true){
+                        $(this).css("border", "solid 2px green");
+                        hasClicked = true;
+                    } else if(answers[i].answer === clicked && answers[i].isAnswer == false){
+                        $(this).css("border", "solid 2px red");
+                        hasClicked = true;
+                    };
+                };
+            };            
+        });
+    } 
 };
     
 function countdown(){
