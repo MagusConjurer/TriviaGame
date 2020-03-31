87667413e5c25dac2cd7565d63eae9cd018c2aba
MagusConjurer <cameronisadavis@gmail.com>
2020-03-30 21:03:36 -0600

    Added shuffleAnswers and runGame. The runGame function handles updating the questions and then showing the score.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 var seconds;
 var running = true;
+var hasClicked = false;
 
 var currentquote = {};
 var selectedQuotes = [];
@@ -11,135 +12,135 @@ quoteOne = {
     quote: "The wheel is come full circle.",
     answers: [
         {answer: "King Lear", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Winter's Tale", isAnswer: false},
+        {answer: "Two Gentlemen of Verona", isAnswer: false},
+        {answer: "Titus Andronicus", isAnswer: false}
     ]
 };
 quoteTwo = {
     quote: "Better three hours too soon than a minute too late.",
     answers: [
         {answer: "The Merry Wives of Windsor", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Troilus and Cressida", isAnswer: false},
+        {answer: "All's Well That Ends Well", isAnswer: false},
+        {answer: "King John", isAnswer: false}
     ]
 };
 quoteThree = {
     quote: "Cowards die many times before their deaths; The valiant never taste of death but once.",
     answers: [
         {answer: "Julius Ceasar", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Anthony and Cleopatra", isAnswer: false},
+        {answer: "Twelfth Night", isAnswer: false},
+        {answer: "Richard II", isAnswer: false}
     ]
 };
 quoteFour = {
     quote: "My words fly up, my thoughts remain below. Words without thoughts never to heaven go.",
     answers: [
         {answer: "Hamlet", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Winter's Tale", isAnswer: false},
+        {answer: "Measure for Measure", isAnswer: false},
+        {answer: "Henry VI, part 1", isAnswer: false}
     ]
 };
 quoteFive = {
     quote: "Brevity is the soul of wit.",
     answers: [
         {answer: "Hamlet", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Henry VI, part 2", isAnswer: false},
+        {answer: "Twelfth Night", isAnswer: false},
+        {answer: "King John", isAnswer: false}
     ]
 };
 quoteSix = {
     quote: "Men of few words are the best men",
     answers: [
         {answer: "Henry V", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Othello", isAnswer: false},
+        {answer: "The Merchant of Venice", isAnswer: false},
+        {answer: "Hamlet", isAnswer: false}
     ]
 };
 quoteSeven = {
     quote: "All that glitters is not gold.",
     answers: [
         {answer: "The Merchant of Venice", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "The Comedy of Errors", isAnswer: false},
+        {answer: "Richard II", isAnswer: false},
+        {answer: "Cymbeline", isAnswer: false}
     ]
 };
 quoteEight = {
     quote: "Love all, trust a few, do wrong to none.",
     answers: [
         {answer: "All's Well That Ends Well", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Hamlet", isAnswer: false},
+        {answer: "Macbeth", isAnswer: false},
+        {answer: "Love's Labours Lost", isAnswer: false}
     ]
 };
 quoteNine = {
     quote: "Our doubts are traitors and make us lose the good we oft might win by fearing to attempt.",
     answers: [
         {answer: "Measure for Measure", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Henry VI, part 1", isAnswer: false},
+        {answer: "Richard III", isAnswer: false},
+        {answer: "Macbeth", isAnswer: false}
     ]
 };
 quoteTen = {
     quote: "I was seeking for a fool when I found you",
     answers: [
         {answer: "As You Like It", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Julius Caesar", isAnswer: false},
+        {answer: "Timon of Athens", isAnswer: false},
+        {answer: "Anthony and Cleopatra", isAnswer: false}
     ]
 };
 quoteEleven = {
     quote: "Out of my sight! Thou dost infect my eyes!",
     answers: [
         {answer: "Richard III", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Much Ado About Nothing", isAnswer: false},
+        {answer: "Love's Labours Lost", isAnswer: false},
+        {answer: "Henry VI, part 3", isAnswer: false}
     ]
 };
 quoteTwelve = {
     quote: "What, you egg!",
     answers: [
         {answer: "Macbeth", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "The Tempest", isAnswer: false},
+        {answer: "Hamlet", isAnswer: false},
+        {answer: "The Merchant of Venice", isAnswer: false}
     ]
 };
 quoteThirteen = {
     quote: "I would challenge you to a battle of wits, but I see you are unarmed.",
     answers: [
         {answer: "The Taming of the Shrew", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Henry VI, part 1", isAnswer: false},
+        {answer: "Romeo and Juliet", isAnswer: false},
+        {answer: "The Merry Wives of Windsor", isAnswer: false}
     ]
 };
 quoteFourteen = {
     quote: "I am not bound to please thee with my answers.",
     answers: [
         {answer: "Merchant of Venice", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "King John", isAnswer: false},
+        {answer: "Othello", isAnswer: false},
+        {answer: "Macbeth", isAnswer: false}
     ]
 };
 quoteFifteen = {
     quote: "Hell is empty. And all the devils are here.",
     answers: [
         {answer: "The Tempest", isAnswer: true},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false},
-        {answer: "", isAnswer: false}
+        {answer: "Measure for Measure", isAnswer: false},
+        {answer: "Henry V", isAnswer: false},
+        {answer: "A Midsummer Night's Dream", isAnswer: false}
     ]
 };
 
@@ -153,8 +154,8 @@ var trivia = {
     // Function to select quote
     startGame : function(){
         this.selectQuotes();
-        this.updatePage(selectedQuotes[0]);
-    
+        this.shuffleAnswers();
+        this.runGame();
     },
 
     selectQuotes : function(){
@@ -171,45 +172,87 @@ var trivia = {
         };
     },
 
+    shuffleAnswers : function(){
+        for(var i=0; i < selectedQuotes.length; i++){
+            for(var j = 0; j < selectedQuotes[i].answers.length; j++){
+                var index = Math.floor(Math.random() * (j));
+                [selectedQuotes[i].answers[j], selectedQuotes[i].answers[index]] = [selectedQuotes[i].answers[index], selectedQuotes[i].answers[j]];  
+            };
+        };
+    },
+
+    runGame : function(){
+        var i = 0;
+        trivia.updatePage(selectedQuotes[i]);
+        countdown();
+        var round = setInterval(function(){
+            i++;
+            if(hasClicked == false){
+                noAnswer++;
+            };
+            if(i === 5){
+                clearInterval(round);
+                trivia.showScore();
+            } else if(i < 5){
+                trivia.updatePage(selectedQuotes[i]);
+                countdown();
+            };
+            hasClicked = false;
+        }, 10000);
+    },
+
     // Function to update page with currentquote details
     updatePage : function(item){
          // Update the quote with selected quote
         $("#quote").html("\"" + item.quote + "\"");
-        $("#answerOne").html(item.answers[0].answer);
-        $("#answerTwo").html(item.answers[1].answer);
-        $("#answerThree").html(item.answers[2].answer);
-        $("#answerFour").html(item.answers[3].answer);
+        $("#answerOne").html(item.answers[0].answer).css("border","none");
+        $("#answerTwo").html(item.answers[1].answer).css("border","none");
+        $("#answerThree").html(item.answers[2].answer).css("border","none");
+        $("#answerFour").html(item.answers[3].answer).css("border","none");
         var answers = item.answers;
-        var hasClicked = false;
         $(".answer").on("click", function(){
             var clicked = $(this).text();
             if(hasClicked == false){
                 for(var i = 0; i < answers.length; i++){
+                    // Check answer and then hasClicked stops from selecting again
                     if(answers[i].answer === clicked && answers[i].isAnswer == true){
                         $(this).css("border", "solid 2px green");
                         hasClicked = true;
+                        correctAnswer++;
                     } else if(answers[i].answer === clicked && answers[i].isAnswer == false){
                         $(this).css("border", "solid 2px red");
                         hasClicked = true;
-                    };
+                        incorrectAnswer++;
+                    }; 
                 };
             };            
         });
-    } 
+    },
+    
+    showScore : function(){
+        $("#quote").remove();
+        $("#answerOne").remove();
+        $("#answerTwo").remove();
+        $("#answerThree").remove();
+        $("#answerFour").remove();
+        var totals = $("<h3>").html("Correct: " + correctAnswer + " Wrong: " + incorrectAnswer + " No Answer: " + noAnswer);
+        $("#timer").append(totals);
+    }
 };
     
 function countdown(){
     seconds = 5;
-    setInterval(function(){
-        if(seconds >= 0){
-            $("#timer").text(seconds);
-            seconds--;
-        }
+    $("#timer").text("Time remaining: " + seconds);
+    var timer = setInterval(function(){
         if(seconds === 0){
-            clearInterval(seconds);
-        }
+            $("#timer").text("Time's Up!");
+            clearInterval(timer);
+        };
+        if(seconds > 0){
+            seconds--;
+            $("#timer").text("Time remaining: " + seconds);
+        };
     },1000);
 };
 
 trivia.startGame();
-countdown();
